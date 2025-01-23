f5ca962d50b8567482e4d62acbceb261a610559d
Lars Johansen <larschrj@users.noreply.github.com>
2025-01-23 14:01:49 +0100

    Add user LED toggle when GDB stepping. Add extern to var a in main.zig so it is added to the symbol table and visible to GDB

--- main.zig
+++ main.zig
@@ -1,5 +1,7 @@
 const chip = @import("STM32F411RE.zig");
 
+export var a: u32 = 3;
+
 export fn main() void {
     chip.RCC.RCC_AHB1ENR.GPIOAEN |= 0x1;
     chip.RCC.RCC_AHB1ENR.GPIOBEN |= 0x1;
@@ -7,5 +9,12 @@ export fn main() void {
     chip.RCC.RCC_AHB1ENR.GPIODEN |= 0x1;
     chip.RCC.RCC_AHB1ENR.GPIOEEN |= 0x1;
     chip.RCC.RCC_AHB1ENR.GPIOHEN |= 0x1;
-    while (true) {}
+
+    chip.GPIOA.MODER |= 0x01 << 10;
+    chip.GPIOA.OTYPER &= ~@as(u16, 0x0 << 5);
+    chip.GPIOA.PUPDR |= 0x10 << 10;
+
+    while (true) {
+        chip.GPIOA.ODR ^= 0x1 << 5;
+    }
 }
