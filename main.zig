27e7b44d3f0fb71850fed0fe6a81e207a1e4be07
Lars Johansen <larschrj@users.noreply.github.com>
2025-01-28 15:50:43 +0100

    Switch to enum literals in main.zig

--- main.zig
+++ main.zig
@@ -5,9 +5,9 @@ export var a: u32 = 3;
 export fn main() void {
     chip.RCC.RCC.RCC_AHB1ENR.GPIOAEN |= 0x1;
 
-    chip.GPIO.GPIOA.MODER.MODER5 = chip.GPIO.GPIO_MODE.output;
-    chip.GPIO.GPIOA.OTYPER.OT5 = chip.GPIO.GPIO_OTYPE.push_pull;
-    chip.GPIO.GPIOA.PUPDR.PUPDR5 = chip.GPIO.GPIO_PUPD.pullup;
+    chip.GPIO.GPIOA.MODER.MODER5 = .output;
+    chip.GPIO.GPIOA.OTYPER.OT5 = .push_pull;
+    chip.GPIO.GPIOA.PUPDR.PUPDR5 = .pullup;
     chip.GPIO.GPIOA.ODR.ODR5 = 0b1;
 
     while (true) {}
