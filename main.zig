a91cee80951b61f0798bf34a215a3877938bfbd3
Lars Johansen <larschrj@users.noreply.github.com>
2025-01-28 22:18:10 +0100

    Change imports in main to shorten statements

--- main.zig
+++ main.zig
@@ -1,14 +1,15 @@
-const chip = @import("STM32F411RE.zig");
+const RCC = @import("STM32F411RE.zig").RCC;
+const GPIO = @import("STM32F411RE.zig").GPIO;
 
 export var a: u32 = 3;
 
 export fn main() void {
-    chip.RCC.RCC.RCC_AHB1ENR.GPIOAEN |= 0x1;
+    RCC.RCC.RCC_AHB1ENR.GPIOAEN |= 0x1;
 
-    chip.GPIO.GPIOA.MODER.MODER5 = .output;
-    chip.GPIO.GPIOA.OTYPER.OT5 = .push_pull;
-    chip.GPIO.GPIOA.PUPDR.PUPDR5 = .pullup;
-    chip.GPIO.GPIOA.ODR.ODR5 = 0b1;
+    GPIO.GPIOA.MODER.MODER5 = .output;
+    GPIO.GPIOA.OTYPER.OT5 = .push_pull;
+    GPIO.GPIOA.PUPDR.PUPDR5 = .pullup;
+    GPIO.GPIOA.ODR.ODR5 = 0b1;
 
     while (true) {}
 }
