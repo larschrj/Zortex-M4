b0e0da93dfcfead4431c510ebb285b2b6054aea5
Lars Johansen <larschrj@users.noreply.github.com>
2025-02-09 14:21:16 +0100

    Change rcc import to directly import rcc field in main

--- main.zig
+++ main.zig
@@ -1,5 +1,5 @@
 const core_cm4 = @import("core_cm4.zig");
-const rcc = @import("stm32f411re_rcc.zig");
+const rcc = @import("stm32f411re_rcc.zig").rcc;
 const gpio = @import("stm32f411re_gpio.zig");
 
 export var a: u32 = 3;
@@ -9,7 +9,7 @@ pub fn main() void {
     core_cm4.enableIrq();
     core_cm4.enableIrqNumber(.TIM1_CC_IRQn) catch unreachable;
 
-    rcc.rcc.rcc_ahb1enr.gpioaen |= 0x1;
+    rcc.rcc_ahb1enr.gpioaen |= 0x1;
 
     gpio.gpioa.moder.moder5 = .output;
     gpio.gpioa.otyper.ot5 = .push_pull;
